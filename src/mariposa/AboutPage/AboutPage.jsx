2f0153d9cbf73636a585c61f53c916710f5aeecc
Apiwat <mrapiiwat@gmail.com>
2024-09-11 23:24:46 +0700

    Refactor routing in App.jsx and add ServicePage component

--- src/mariposa/AboutPage/AboutPage.jsx
+++ src/mariposa/AboutPage/AboutPage.jsx
@@ -1,12 +1,17 @@
-import Nav from "../../component/Nav/Nav"
+import Nav from "../../component/Nav/Nav";
 
 const AboutPage = () => {
   return (
     <>
       <Nav />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto beatae id modi, autem voluptate veniam? Quisquam quasi quos, sapiente numquam nulla atque corporis exercitationem! Quo illo reiciendis quibusdam similique blanditiis.</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+        beatae id modi, autem voluptate veniam? Quisquam quasi quos, sapiente
+        numquam nulla atque corporis exercitationem! Quo illo reiciendis
+        quibusdam similique blanditiis.
+      </p>
     </>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
