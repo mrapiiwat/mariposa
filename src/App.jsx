dcad56f863a81c61814f697acc911fb38cd2ce5f
Apiwat <mrapiiwat@gmail.com>
2024-09-12 16:56:54 +0700

    Update dependencies: Add react-spinners package

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import AboutPage from "./mariposa/AboutPage/AboutPage";
 import ServicePage from "./mariposa/ServicePage/ServicePage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import GameRoom from "./mariposa/GameRoom/GameRoom";
+import { useState, useEffect } from "react";
+import PulseLoader from "react-spinners/PulseLoader";
+import "./App.css";
+import Logo from "./assets/mariposalogo1.png";
 
 const router = createBrowserRouter([
   {
@@ -24,10 +28,29 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
+  
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <div className="App">
+      {loading ? (
+        <div className="load-con">
+          <img className="Applogo" src={Logo} alt="" />
+          <PulseLoader className="loader" loading={loading} size={5} color="#00408c"/>
+        </div>
+      ) : (
+        <div className="fade-content">
+          <RouterProvider router={router} />
+        </div>
+        
+      )}
+    </div>
   );
 }
 
--- src/mariposa/HomePage/HomePage.jsx
+++ src/mariposa/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import Nav from "../../component/Nav/Nav";
 import "./HomePage.css";
 
 const HomePage = () => {
+
   return (
     <div className="home-page-con">
       <Nav />
