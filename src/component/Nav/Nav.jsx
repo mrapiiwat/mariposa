e646ecc97a2635962a43bdca21704c3d3899e067
Apiwat <mrapiiwat@gmail.com>
2024-09-20 08:35:05 +0700

    Refactor Nav component: Add loading state and loader component

--- src/component/Nav/Nav.jsx
+++ src/component/Nav/Nav.jsx
@@ -1,40 +1,73 @@
 import "./nav.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import mainlogo from "../../assets/libarylogo.png";
 
 const Nav = () => {
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className="nav-con">
-      <nav>
-        <div className="logo-con">
-          <Link to="/">
-            <img className="main-logo" src={mainlogo} alt="main logo" />
-          </Link>
-        </div>
-        <div className="menu-con">
-          <ul className="ul-nav">
-            <li className="li-nav">
-              <Link className="menu-a" to="/about">
-                เกี่ยวกับเรา
-              </Link>
-            </li>
-            <li className="li-nav">
-              <Link className="menu-a" to="/service">
-                บริการ
-              </Link>
-            </li>
-          </ul>
-          <div className="lang-con">
-            <Link className="lang-a" to="">
-              ไทย
+      {loading ? (
+        <Loader loading={loading} />
+      ) : (
+        <nav>
+          <div className="logo-con">
+            <Link
+              onClick={() => {
+                loading(true);
+                setTimeout(() => {
+                  setLoading(false);
+                }, 1000);
+              }}
+              to="/"
+            >
+              <img className="main-logo" src={mainlogo} alt="main logo" />
             </Link>
-            <Link className="lang-a" to="">
-              ENG
-            </Link>
-            <span className="just-span">|</span>
           </div>
-        </div>
-      </nav>
+          <div className="menu-con">
+            <ul className="ul-nav">
+              <li className="li-nav">
+                <Link className="menu-a" to="/about">
+                  เกี่ยวกับเรา
+                </Link>
+              </li>
+              <li className="li-nav">
+                <Link className="menu-a" to="/service">
+                  บริการ
+                </Link>
+              </li>
+            </ul>
+            <div className="lang-con">
+              <Link
+                onClick={() => {
+                  loading(true);
+                  setTimeout(() => {
+                    setLoading(false);
+                  }, 1500);
+                }}
+                className="lang-a"
+                to=""
+              >
+                ไทย
+              </Link>
+              <Link
+                onClick={() => {
+                  loading(true);
+                  setTimeout(() => {
+                    setLoading(false);
+                  }, 1500);
+                }}
+                className="lang-a"
+                to=""
+              >
+                ENG
+              </Link>
+              <span className="just-span">|</span>
+            </div>
+          </div>
+        </nav>
+      )}
     </div>
   );
 };
--- src/component/Nav/NavLogin.jsx
+++ src/component/Nav/NavLogin.jsx
@@ -4,8 +4,10 @@ import userPic from "../../assets/user.png";
 import mainlogo from "../../assets/libarylogo.png";
 import { MdOutlineExpandMore } from "react-icons/md";
 import { useState } from "react";
+import Loader from "../Loader/Loader";
 
 const NavLogin = () => {
+  const [loading, setLoading] = useState(false);
   const [user, setUser] = useState([
     {
       name: "Guest",
@@ -15,33 +17,55 @@ const NavLogin = () => {
 
   return (
     <div className="nav-log-con">
-      <nav>
-        <div className="logo-con">
-          <Link to="/">
-            <img className="main-logo" src={mainlogo} alt="main logo" />
-          </Link>
-        </div>
-        <div className="menu-con">
-          <ul className="ul-nav-log">
-            <li className="li-nav-log">
-              <img className="userPic" src={user[0].img} alt="" />
-              <div className="user-con">
-                <p className="p-name">{user[0].name}</p>
-                <MdOutlineExpandMore className="expand" />
-              </div>
-            </li>
-          </ul>
-          <div className="lang-con">
-            <Link className="lang-a" to="">
-              ไทย
+      {loading ? (
+        <Loader loading={loading} />
+      ) : (
+        <nav>
+          <div className="logo-con">
+            <Link to="/">
+              <img className="main-logo" src={mainlogo} alt="main logo" />
             </Link>
-            <Link className="lang-a" to="">
-              ENG
-            </Link>
-            <span className="just-span">|</span>
           </div>
-        </div>
-      </nav>
+          <div className="menu-con">
+            <ul className="ul-nav-log">
+              <li className="li-nav-log">
+                <img className="userPic" src={user[0].img} alt="" />
+                <div className="user-con">
+                  <p className="p-name">{user[0].name}</p>
+                  <MdOutlineExpandMore className="expand" />
+                </div>
+              </li>
+            </ul>
+            <div className="lang-con">
+              <Link
+                onClick={() => {
+                  setLoading(true);
+                  setTimeout(() => {
+                    setLoading(false);
+                  }, 1500);
+                }}
+                className="lang-a"
+                to=""
+              >
+                ไทย
+              </Link>
+              <Link
+                onClick={() => {
+                  setLoading(true);
+                  setTimeout(() => {
+                    setLoading(false);
+                  }, 1500);
+                }}
+                className="lang-a"
+                to=""
+              >
+                ENG
+              </Link>
+              <span className="just-span">|</span>
+            </div>
+          </div>
+        </nav>
+      )}
     </div>
   );
 };
