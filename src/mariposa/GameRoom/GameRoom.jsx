4453cae43bfdc5fcbee9d2da1340eb7c413734a7
cream <sukhatai7331@gmail.com>
2024-09-21 21:47:49 +0700

    ทำรูปbackgroudห้องเล่นเกมส์

--- src/mariposa/GameRoom/GameRoom.jsx
+++ src/mariposa/GameRoom/GameRoom.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import NavLogin from "../../component/Nav/NavLogin";
 import "./GameRoom.css";
+import GameRoomImg from "../../assets/gameroom.jpg";
 
 const GameRoom = () => {
   return (
     <div className="game-room-con">
       <NavLogin />
-      <h1>Cream</h1>
+      <div className="gr">
+        <div className="gameroomheader">
+          <img className="gamepic" src={GameRoomImg} alt="" />
+          <div className="blurgamepic"></div>
+          <div className="gameroomname">ห้องเล่นเกม</div>
+        </div>
+      </div>
     </div>
   );
 };
