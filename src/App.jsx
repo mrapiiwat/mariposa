1d91c581e2d5eb4569639607fc1ffdaff85ce26c
Naruemon55 <65112125@dpu.ac.th>
2024-09-23 10:06:28 +0700

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import MemberPage from "./mariposa/AboutPage/MemberPage/MemberPage";
 import Loader from "./component/Loader/Loader";
+import LoginPage from "./mariposa/LoginPage/LoginPage.jsx";
 
 const LanguageWrapper = ({ children }) => {
   const { lang } = useParams(); // ดึงภาษาออกจาก URL path
@@ -68,6 +69,14 @@ const router = createBrowserRouter([
     element: (
       <LanguageWrapper>
         <MemberPage />
+      </LanguageWrapper>
+    ),
+  },
+  {
+    path: "/:lang/login",
+    element: (
+      <LanguageWrapper>
+        <LoginPage/>
       </LanguageWrapper>
     ),
   },
---
+++ src/mariposa/LoginPage/LoginPage.jsx
@@ -0,0 +1,30 @@
+import {useState} from "react";
+import Nav from "../../component/Nav/Nav";
+import "./LoginPage.css";
+
+function LoginPage() {
+
+
+
+  return (                                                          
+    <div className="Login-Page-con">
+      <Nav/>
+      <div className="Loginpage">
+        <div className="Login-page-input">
+          
+
+        </div>
+      
+
+      </div>
+
+      
+
+    </div>
+  )
+}
+
+
+
+
+export default LoginPage
