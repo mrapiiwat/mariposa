97c17afb1569a61ebfef7456e92209f4042a689f
Naruemon55 <65112125@dpu.ac.th>
2024-09-23 23:13:55 +0700

    update login

--- src/mariposa/LoginPage/LoginPage.jsx
+++ src/mariposa/LoginPage/LoginPage.jsx
@@ -1,30 +1,35 @@
-import {useState} from "react";
+import { useState } from "react";
 import Nav from "../../component/Nav/Nav";
 import "./LoginPage.css";
+import { Link } from "react-router-dom";
 
 function LoginPage() {
-
-
-
-  return (                                                          
+  return (
     <div className="Login-Page-con">
-      <Nav/>
-      <div className="Loginpage">
-        <div className="Login-page-input">
+      <Nav />
+      <div className="login-page">
+        <div className="login-box-con">
+          <div className="blur-login-backgound"></div>
+          <div className="login-box">
+            <div className="userinput-con">
+              <label>ชื่อผู้ใช้งาน หรือ อีเมล</label>
+              <input type= "text" />
+            </div>
+
+            <div className="passwordinput-con">
+              <label>รหัสผ่าน</label>
+              <input type= "password" />
+            </div>
+            <div className="login-btn-con">
+              <button className="log-btn">เข้าสู่ระบบ</button> 
+              <Link className="nolog-btn">ยังไม่ได้เป็นสมาชิก?</Link>
+            </div>
+          </div>
           
-
         </div>
-      
-
       </div>
-
-      
-
     </div>
-  )
+  );
 }
 
-
-
-
-export default LoginPage
+export default LoginPage;
