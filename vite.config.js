f54259a934ee50f3492924b5b18917f9555459ab
Apiwat <mrapiiwat@gmail.com>
2024-09-24 11:20:05 +0700

    Refactor server configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// export default defineConfig({
+//   plugins: [react()],
+  
+// })
+
+export default {
+  server: {
+    host: true,
+    port: 3000, // หรือพอร์ตที่คุณต้องการ
+  }
+}
