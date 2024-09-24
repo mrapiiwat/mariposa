fcdd15d8e63b866607e602bb01b2f36ba273f2fa
piyathida <142969951+piyathidaaa@users.noreply.github.com>
2024-09-24 10:36:25 +0700

    fai

--- src/mariposa/ConferenceRoom/ConferenceRoom.jsx
+++ src/mariposa/ConferenceRoom/ConferenceRoom.jsx
@@ -2,14 +2,16 @@ import NavLogin from "../../component/Nav/NavLogin";
 import RoomBackground from "../../component/RoomBackground/RoomBackground";
 import "./ConferenceRoom.css"
 import backgroung from '../../assets/conferenceroom.jpg'
+import { useTransition } from "react-i18next";
 
 const ConferenceRoom = () => {
+    const {t} = useTransition();
 
 return (
     
     <div className="confer-room-con">
         <NavLogin/>
-        <RoomBackground imgOfroom={backgroung} nameofroom={'ห้องประชุม'}/>
+        <RoomBackground imgOfroom={backgroung} nameofroom={t('conference-title')}/>
         
     </div>
 );
