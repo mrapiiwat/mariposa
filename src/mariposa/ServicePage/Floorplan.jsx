5ada66b22724c0b7ad0a6e9782476913b95b1e33
Apiwat <mrapiiwat@gmail.com>
2024-09-25 22:23:15 +0700

    Refactor servicefee_detail class in Floorplan.css and Floorplan.jsx

--- src/mariposa/ServicePage/Floorplan.jsx
+++ src/mariposa/ServicePage/Floorplan.jsx
@@ -11,28 +11,28 @@ const Floorplan = () => {
     <div className='servicefee_con'>
         <Nav />
         <div className='servicefee'>
-            <div className='servicefee_detail'>
+            <div className='servicefee_detailabc'>
                 <div className='servicefee_title'>ชั้น 1: <br></br> ห้องสมุดหลักบรรยากาศเงียบสงบ</div>
             </div>
             <div className='floor1'>
                     <img src={Floor1} className='picfloor1' alt='floor1' />
             </div>
 
-            <div className='servicefee_detail'>
+            <div className='servicefee_detailabc'>
                 <div className='servicefee_title'>ชั้น 2: <br></br> พื้นที่ Co-working Space</div>
             </div>
             <div className='floor1'>
                     <img src={Floor2} className='picfloor1' alt='floor1' />
             </div>
 
-            <div className='servicefee_detail'>
+            <div className='servicefee_detailabc'>
                 <div className='servicefee_title'>ชั้น 3: <br></br> ห้องประชุมขนาดต่างๆ</div>
             </div>
             <div className='floor1'>
                     <img src={Floor3} className='picfloor1' alt='floor1' />
             </div>
             
-            <div className='servicefee_detail'>
+            <div className='servicefee_detailabc'>
                 <div className='servicefee_title'>ชั้น 4: <br></br> โรงภาพยนตร์ขนาดเล็ก ห้องเกม และพื้นที่พักผ่อน</div>
             </div>
             <div className='floor1'>
